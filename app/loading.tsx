1632cd932e6e71f465ed0e1408d6bd8cb95e0562
ngumi22 <mwaurapngumi@gmail.com>
2024-08-30 17:26:00 +0300

    Create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,15 @@
+// app/loading.tsx
+
+export default function Loading() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}>
+      <h1>Loading...</h1>
+    </div>
+  );
+}
